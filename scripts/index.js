a2ac3e3d378dfca7c10c665c1863615157ee72e3
Evan Vogts <evanjvogts@gmail.com>
2019-09-25 13:26:47 -0500

    bug test

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 import shoppingList from './shopping-list.js';
 
-const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
-  const itemNames = ['apples', 'pears' ];
-  itemNames.forEach(name => {
-  try {
-    item.validateName(name);
-    // create a new item if name is valid
-    store.items.push(item.create(name));
-  } catch(error) {
-    console.log(`Cannot add item: ${error.message}`);
-  }
-});
-};
+// const main = function () {
+//   shoppingList.bindEventListeners();
+//   shoppingList.render();
+//   const itemNames = ['apples', 'pears' ];
+//   itemNames.forEach(name => {
+//   try {
+//     item.validateName(name);
+//     // create a new item if name is valid
+//     store.items.push(item.create(name));
+//   } catch(error) {
+//     console.log(`Cannot add item: ${error.message}`);
+//   }
+// });
+// };
 
 $(main);
 
--- scripts/store.js
+++ scripts/store.js
@@ -1,10 +1,44 @@
 const items = []
 const hideCheckedItems = false
 
+//returns the object
+function findById (id) {
+  id.find (element => element.id === id);
+}
+
+function addItem (name) {
+  try {
+    item.validateName(name);
+    store.items.push(this.items.create(name));
+  } catch(error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+function findAndToggleChecked (id) {
+  const foundItem = this.findById(id);
+  foundItem.checked = !foundItem.checked;
+};
+
+function findAndUpdateName (id, newName) {
+  try {
+    item.validateName(newName);
+    const object = this.findById(id);
+    object.name = newName;
+  }
+  catch (error) {console.log(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete (id) {
+  //const foundItem = this.findById(id);
+  items = items.filter (element => element.id !== id);
+}
+
 
 import item from './item.js';
 export default {
-    items,
-    hideCheckedItems
+  items,
+  hideCheckedItems
 };
 
